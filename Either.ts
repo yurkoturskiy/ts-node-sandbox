63a9d1dabc761c9eceb744a98039394a96994c9e
Yurko Turskiy <guandjoy@gmail.com>
2020-04-13 23:49:00 +0300

    Update Either.ts

--- Either.ts
+++ Either.ts
@@ -1,11 +1,14 @@
 import { Either, left, right } from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/pipeable";
 
-const setArray = (): number[] => Array(4).fill(1);
+const setArray = (numOfElements: number): number[] =>
+  Array(numOfElements).fill(1);
 
 const validate = (data: number[]): Either<Error, number[]> =>
   data.length === 4 ? right(data) : left(new Error("Wrong format"));
 
-const parseArray = (): Either<Error, number[]> => pipe(setArray(), validate);
+const parseArray = (numOfElements: number): Either<Error, number[]> =>
+  pipe(setArray(numOfElements), validate);
 
-parseArray();
+const a = parseArray(5);
+console.log(a);
