192ffd944f3f63384a13bcad67fa508f3defd398
Yurko Turskiy <guandjoy@gmail.com>
2020-04-18 23:57:16 +0300

    Create IO.ts example. Read write localStorage

---
+++ IO.ts
@@ -0,0 +1,9 @@
+// read / write localStorage
+
+import { fromNullable, Option } from "fp-ts/lib/Option";
+
+const getItem = (key: string): IO<Option<string>> => () =>
+  fromNullable(localStorage.getItem(key));
+
+const setItem = (key: string, value: string): IO<void> => () =>
+  localStorage.setItem(key, value);
